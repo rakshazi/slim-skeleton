e159b53dbebfd66eba9b3c23ca58858b5d72b6eb
Nikita Chernyi <rakshazi@users.noreply.github.com>
2017-02-20 15:28:20 +0200

    Added dynamic config for phinx

---
+++ app/config/phinx.php
@@ -0,0 +1,23 @@
+<?php
+$core = require __DIR__ . '/core.php';
+$db = $core['settings']['database'];
+
+return [
+    'paths' => [
+        'migrations' => __DIR__ . '/../migrations',
+        'seeds' => __DIR__ . '/../seeds'
+    ],
+
+    'environments' => [
+        'default_database' => 'default',
+        'default' => [
+            'name' => $db['database_name'],
+            'host' => $db['server'],
+            'user' => $db['username'],
+            'pass' => $db['password'],
+            'port' => $db['port'],
+            'charset' => $db['charset'],
+            'adapter' => $db['database_type'],
+        ],
+    ],
+];
