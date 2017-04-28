c365a13a3e7dfae3daf5f06c03ccccf31f06e695
Nikita Chernyi <rakshazi@users.noreply.github.com>
2017-04-28 16:10:04 +0300

    docker: removed sleep delay, removed cd

--- docker/run.sh
+++ docker/run.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 cd /var/lib/nginx/html
 composer update  --ignore-platform-reqs --no-ansi --no-dev --no-interaction --no-progress --no-scripts --optimize-autoloader
-cd app
-sleep 15
 migrate
 exec su-exec root:root /bin/s6-svscan /etc/s6.d
